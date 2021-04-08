130834c4cec385c2715537b963300382350177aa
prajaktabramhe <prajakta14bramhe@gmail.com>
2021-04-08 18:06:02 +0530

    [Prajakta]: Added: test case for retrieve the data

--- src/test/java/com/bridgelabz/PayrollServiceDBTest.java
+++ src/test/java/com/bridgelabz/PayrollServiceDBTest.java
@@ -1,2 +1,19 @@
-package com.bridgelabz;public class PayrollServiceDBTest {
+package com.bridgelabz;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+public class PayrollServiceDBTest
+{
+
+@Test
+    public  void givenEmployeePayrollDB_WhenRetrieved_ShouldMatchEmployeeCount()
+    {
+        EmployeePayrollService employeePayrollService = new EmployeePayrollService();
+        List<EmployeePayrollData> employeePayrollData = employeePayrollService.readEmployeePayrollData(EmployeePayrollService.IOService.DB_IO);
+        Assertions.assertEquals(3, employeePayrollData.size());
+    }
+
 }
